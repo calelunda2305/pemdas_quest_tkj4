dd85da8024b1da97bcda3250e7e0a6ed05406053
calelunda2305 <112527160+calelunda2305@users.noreply.github.com>
2022-09-05 08:38:47 +0700

    Add files via upload

---
+++ python5-tkj4.py
@@ -0,0 +1,26 @@
+#dolar ke rupiah
+
+print ("mata uang dolar ke rupiah")
+dolar =int(input("dolar = "))
+
+rupiah = dolar * 14.897
+print ("rupiah=", rupiah)
+
+
+#euro ke yen
+
+print ("mata uang euro ke yen")
+euro= int(input("euro = "))
+
+yen = euro * 139,16
+print ("yen=", yen)
+
+#won ke rupiah
+
+print ("mata uang won ke rupiah")
+won =int(input("won = "))
+
+rupiah = won * 10,92
+print ("rupiah=", rupiah)
+
+
