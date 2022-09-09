344caab873e25ab6e36b2199b0c8a83489982690
calelunda2305 <112527160+calelunda2305@users.noreply.github.com>
2022-09-09 14:48:41 +0700

    Add files via upload

---
+++ UPRAK - Lu'luul Mukaromah - TKJ4.py
@@ -0,0 +1,36 @@
+#balok
+
+panjang = 10
+lebar = 15
+tinggi = 15
+volume_b = panjang * lebar * tinggi
+print("volume balok adalah", volume_b)
+
+#kubus
+
+sisi1 = 20
+sisi2 = 15
+sisi3 = 25
+volume_k = sisi1 * sisi2 * sisi3
+print("volume kubus adalah", volume_k)
+
+#limas 1/3 * la * t
+la = 30
+t = 15
+volume_l = la * t / 3
+print("volume limas adalah", volume_l)
+
+#tabung phi * r * r * t
+r = 60
+t = 20
+phi = 22/7
+volume_t = phi * r * r * t
+print("volume tabung adalah", volume_t)
+
+
+#celcius ke reamur
+print ("celcius ke reamur")
+celcius =int(input("celcius = "))
+
+reamur = celcius * 4
+print ("reamur=", reamur)
