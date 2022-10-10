9511c31792f0f745668fc961bb74b3f48b482efa
calelunda2305 <112527160+calelunda2305@users.noreply.github.com>
2022-10-10 09:51:17 +0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,27 @@
+# Membuat Tipe Data Koleksi
+
+koleksi_data_str = ["Pisang", "Mangga", "Jambu", "Semangka"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["Rizky billar", 100, True, "Reza Arap"]
+
+print("Koleksi data String:", koleksi_data_str)
+
+print("Koleksi data Integer:", koleksi_data_int)
+
+print("Koleksi data Campuran", koleksi_data_mix)
+
+# Buatlah 3 Kumpulan data: Nama Hewan, Nilai UTS, Nama Teman sebangku kalian
+
+nama_hewan= ["Kucing", "Kelinci", "Kangguru"]
+
+nilai_UTS = [100, 90, 100]
+
+koleksi_data_mix = ["Burung", 90, "Nazla"]
+
+print("Nama Hewan:", nama_hewan)
+
+print("Nilai UTS:", nilai_UTS)
+
+print("Koleksi data Campuran:", koleksi_data_mix)
